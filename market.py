afcc0c5594651624b4173d6cc0dbeacef9e3b501
Rachis Vsevolod <seva-ra4is@mail.ru>
2023-08-31 10:44:11 +0700

    Update market.py

--- market.py
+++ market.py
@@ -4,29 +4,30 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import ActionChains
+from selenium.webdriver.common.by import By
 
-url = r"https://vk.com/market-180581210"
+url = r"https://vk.com/market-213409010"
 vk_url = r"https://vk.com"
 
 driver = webdriver.Chrome("chromedriver.exe")
 driver.get(url)
 
-input()
+#input()
 soup = BeautifulSoup(driver.page_source, 'lxml')
 
 # get all products
 for i, product_url in enumerate([row.find("a")["href"] for row in soup.find_all('div', class_='market_row')]):
     # wait while data loads
-    market_item_description = None
-    while market_item_description is None:
-        driver.get(vk_url + product_url)
-        print(vk_url + product_url)
-        sleep(1)
-        soup = BeautifulSoup(driver.page_source, 'lxml')
-        market_item_description = soup.find("div", id="market_item_description")
+    driver.get(vk_url + product_url)
+    print(vk_url + product_url)
+    sleep(5)
+    soup = BeautifulSoup(driver.page_source, 'lxml')
+    title = soup.find("h1", class_="ItemName").text.strip()
+    market_item_description = soup.find("div", class_="ItemDescription")
+    description = f"{title}\n{market_item_description.text.strip()}" if market_item_description else ""
 
-    title = soup.find("div", class_="market_item_title").text.strip()
-    description = f"{title}\n{market_item_description.text.strip()}"
+    print(title)
+    print(description)
 
     # create folder
     if not os.path.exists(str(i)):
@@ -36,11 +37,11 @@ for i, product_url in enumerate([row.find("a")["href"] for row in soup.find_all(
         handler.write(description.encode())
 
     # for all image
-    for number, _ in enumerate(soup.find_all("div", class_="market_item_thumb")):
-        # hover cropped image, reload html and save image
-        ActionChains(driver).move_to_element(driver.find_element("id", f'market_item_thumb{number}')).perform()
+    for number, _ in enumerate(soup.find_all("div", class_="ItemGallery__thumb")):
+        ActionChains(driver).move_to_element(driver.find_elements(By.CLASS_NAME, 'ItemGallery__thumb')[number]).perform()
+        sleep(5)
         soup = BeautifulSoup(driver.page_source, 'lxml')
-        img_link = soup.find("img", id="market_item_photo")["src"].replace("amp;", "")
+        img_link = soup.find("img", class_="ItemGallery__image")["src"].replace("amp;", "")
         img_data = requests.get(img_link).content
         with open(f'{i}/{number}.jpg', 'wb') as handler:
             handler.write(img_data)
